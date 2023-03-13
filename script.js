fd3d3c92acc04f1f62a49d8a3b6141ad325a300d
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-13 06:57:10 +0000

    add js check day in week

---
+++ script.js
@@ -0,0 +1,19 @@
+let dayNumber = 0;
+
+if(dayNumber === 0){
+    console.log('This is sunday')
+}else if(dayNumber === 1){
+    console.log('This is monday')
+}else if (dayNumber === 2){
+    console.log('This tuesday')
+}else if(dayNumber === 3){
+    console.log('This is Wesday')
+}else if(dayNumber === 4){
+    console.log('this is thursday')
+}else if(dayNumber === 5){
+    console.log('this is friday')
+}else if(dayNumber === 6){
+    console.log('this is Saturday')
+}else{
+    console.log('Invalid day number')
+}
