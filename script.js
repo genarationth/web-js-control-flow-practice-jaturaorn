0e5058b0b5b7e998301a43c12e9db15555b7c77f
jaturaporn <83279262+jaturaorn@users.noreply.github.com>
2023-03-13 07:44:15 +0000

    add Ternary Operator

--- script.js
+++ script.js
@@ -1,19 +1,32 @@
-const dayNumber = 8;
+// const dayNumber = 8;
 
-if(dayNumber === 0){
-    console.log('This is sunday')
-}else if(dayNumber === 1){
-    console.log('This is monday')
-}else if (dayNumber === 2){
-    console.log('This tuesday')
-}else if(dayNumber === 3){
-    console.log('This is Wesday')
-}else if(dayNumber === 4){
-    console.log('this is thursday')
-}else if(dayNumber === 5){
-    console.log('this is friday')
-}else if(dayNumber === 6){
-    console.log('this is Saturday')
-}else{
-    console.log('Invalid day number')
-}
+// if(dayNumber === 0){
+//     console.log('This is sunday')
+// }else if(dayNumber === 1){
+//     console.log('This is monday')
+// }else if (dayNumber === 2){
+//     console.log('This tuesday')
+// }else if(dayNumber === 3){
+//     console.log('This is Wesday')
+// }else if(dayNumber === 4){
+//     console.log('this is thursday')
+// }else if(dayNumber === 5){
+//     console.log('this is friday')
+// }else if(dayNumber === 6){
+//     console.log('this is Saturday')
+// }else{
+//     console.log('Invalid day number')
+// }
+
+const password = 'pas$word';
+
+password = password.length >= 12 ? console.log('password is very strong so Welcome!!'):
+console.log('SORRY!! the password is too short');
+
+password = password.length >= 8 ? console.log('Welcome!!'):
+console.log('SORRY!! the password is too short');
+
+password = password.length >= 12 ? console.log('password is very strong so Welcome!!'):
+password = password.length >= 8 ? console.log('Welcome!!'):
+console.log('SORRY!! the password is too short');
+
